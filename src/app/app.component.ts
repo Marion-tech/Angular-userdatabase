b3379f4cc1bc5d42c7eb470314dd5e1468e41417
Marion-tech <116389134+Marion-tech@users.noreply.github.com>
2023-01-24 18:08:21 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { isFunction } from 'rxjs/internal/util/isFunction';
 import { dataFirstNames } from './models/data.names';
 import { IUser, IUserList } from './models/interfaces';
 
@@ -12,11 +13,15 @@ export class AppComponent {
   public nomprenom: IUserList = {
     userList: [],
   };
-  public homonymelist: string[] = [];
+  public homonymelist: number[] = [];
 
   constructor() {
-    this.generateUsers(25);
+    this.generateUsers(20);
     this.homonyme();
+  }
+
+  public checkHomonyme(id) : boolean{
+    return !!this.homonymelist.find((cherche:number)=> id === cherche);
   }
 
   private generateUsers(nb: number): void {
@@ -38,12 +43,46 @@ export class AppComponent {
   }
 
   private homonyme(): void {
-    let concat = this.nomprenom.userList.map((user:IUser) => user.id + ' - '+ user.firstname+user.name).sort((a,b)=> a.firstname > b.firstname ? 1 : -1);
-    console.log('concat', concat);
+    let concat: IUser[] = this.nomprenom.userList.sort((a, b) =>
+      a.firstname + a.name > b.firstname + b.name ? 1 : -1
+    );
 
+    concat.forEach((user: IUser, i) => {
+      
+      if (concat[i+1] && (user.name+user.firstname) === (concat[i+1].name
+        +concat[i + 1].firstname)) {
+          this.homonymelist.push(user.id);
+          this.homonymelist.push(concat[i+1].id);
+      }
+    });
+    console.log(
+      'concat', this.nomprenom.userList.sort((a,b)=> a.id > b.id ? 1 : -1)
+    );
+
+    console.log('homonyme', this.homonymelist);
     //this.homonymelist = concat.filter((name1:string)=> this.homonymelist.includes(name1) )
     //this.homonymelist = concat.find((name:string)=> concat.find((name2:string)=> name===name2))
-    console.log(this.homonymelist);
-    //return;
+  }
+
+  private training() {
+    let example = [1, 2, 3, 3, 4, 4, 5, 8, 9, 9];
+    let sansDoublons = [];
+    //example = example.filter((filt:number, i)=> filt[i] === filt[i+1]);
+    //example = example.filter((filt:number, i)=> example.indexOf(filt)===i);
+    for (let i = 0; i < example.length - 1; i++) {
+      if (example[i] != example[i + 1]) {
+        sansDoublons.push(example[i]);
+      }
+    }
+    console.log('For sans doublons', sansDoublons);
+
+    let newtab = [];
+    example.forEach((num, i) => {
+      if (num != example[i + 1]) {
+        newtab.push(num);
+      }
+    });
+    console.log('Foreach sans doublons :', newtab);
+    console.log('Filter sans doublons ?');
   }
 }
